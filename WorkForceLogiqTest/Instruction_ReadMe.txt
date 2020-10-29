6ee327f15d9deb5f55c28f9fa744082348af46a3
tejash patel <tejash.devdigital@gmail.com>
2020-10-29 22:07:13 +0530

    Added application for reverse and length of the string.
    - C# console application to get the reverse and the length of the enter string.

---
+++ WorkForceLogiqTest/Instruction_ReadMe.txt
@@ -0,0 +1,17 @@
+Step 1:- 
+- Enter any string, word, string or number as a first parameter.
+- E.g., If user enters:-  "This is Tejash Patel, Reset@123" in the application.
+
+Step 2:-
+- Enter either "-r" or "-l" as second parameter.
+
+Scenario 1:- If user enter "-r" then the output will be shown in reverse order.
+
+Output:- 
+Reverse string is: 321@teseR ,letaP hsajeT si sihT 
+
+Scenario 2:- If user enter "-l" then the output will shown the length of the enter text.
+
+Output:- 
+The length of string is: 31
+
---
+++ WorkForceLogiqTest/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkForceLogiqTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strInput = "", strOperation="",strReverse="-r",strLength ="-l",strReverseOutput="";
+
+            // Display title for C# console application.
+            Console.WriteLine("Console Application to show length and reverse string in C#\r");
+            Console.WriteLine("--------------------------------------------------------------\n");
+
+            // Ask the user to input the string.
+            Console.WriteLine("Please enter the string, word or sentence, and then press Enter:\n");
+            strInput = Convert.ToString(Console.ReadLine());
+
+            // Ask the user to input the -r or -l parameter.
+            Console.WriteLine("\nPlease enter the parameter (–r) or (–l), and then press Enter:\n");
+            strOperation = Convert.ToString(Console.ReadLine());
+
+            //For (-r) reverse parmeters
+            if (strOperation.ToLower().Equals(strReverse))
+            {
+                int length = 0;
+                length = strInput.Length - 1;
+                while (length >= 0)
+                {
+                    strReverseOutput = strReverseOutput + strInput[length];
+                    length--;
+                }
+                //Displaying the reverse word  
+                Console.WriteLine("\n\nReverse string is: " + strReverseOutput);
+                Console.ReadLine();
+            }
+
+            //For (-l) length parmeters
+            if (strOperation.ToLower().Equals(strLength.ToLower()))
+            {   
+                //Displaying the string length.  
+                Console.WriteLine("\nThe length of string is: " + strInput.Length);
+                Console.ReadLine();
+            }
+        }
+    }
+}
